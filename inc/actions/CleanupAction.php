29efdaf6ebaea7b933b384b38fe312a93dd09ccc
Marc Fallows <marc@blinkbox.com>
2013-10-22 14:02:57 +0100

    Run cleanup based on heartbeat  - Cleanup runs based on heartbeat, rather than client pings.

--- inc/actions/CleanupAction.php
+++ inc/actions/CleanupAction.php
@@ -20,50 +20,56 @@ class CleanupAction extends Action {
 
 		$resetTimedoutRuns = 0;
 
-		// Get clients that are considered disconnected (not responding to the latest pings).
-		// Then mark the runresults of its active runs as timed-out, and reset those runs so
-		// they become available again for different clients in GetrunAction.
+		$now = time();
+		$maxHeartbeatAge = $now - $conf->client->runHeartbeatTimeMargin;
 
 		$rows = $db->getRows(str_queryf(
 			"SELECT
-				runresults.id as id
-			FROM
-				runresults
-			INNER JOIN clients ON runresults.client_id = clients.id
-			WHERE runresults.status = 1
-			AND   clients.updated < %s;",
-			swarmdb_dateformat( Client::getMaxAge( $context ) )
+				id,
+				run_id
+			FROM runresults
+			WHERE status = %u
+				AND next_heartbeat < %s;",
+			ResultAction::$STATE_BUSY,
+			swarmdb_dateformat( $maxHeartbeatAge )
 		));
 
-		if ($rows) {
-			foreach ($rows as $row) {
-				// Reset the run
+		if ( $rows ) {
+			foreach ( $rows as $row ) {
+
+				$runID = $row->run_id;
+				$resultsID = $row->id;
+
 				$ret = $db->query(str_queryf(
-					"UPDATE run_useragent
+					'UPDATE
+						run_useragent
 					SET
-						status = 0,
-						results_id = NULL
-					WHERE results_id = %u;",
-					$row->id
+						completed = completed + 1,
+						status = IF(completed + 1 < max, 0, 2),
+						updated = %s
+					WHERE results_id = %u
+					LIMIT 1;',
+					swarmdb_dateformat( SWARM_NOW ),
+					$resultsID
 				));
 
-				// If the previous UPDATE query failed for whatever
-				// reason, don't do the below query as that will lead
-				// to data corruption (results with state LOST must never
-				// be referenced from run_useragent.results_id).
 				if ( $ret ) {
-					// Update status of the result
 					$ret = $db->query(str_queryf(
-						"UPDATE runresults
-						SET status = %s
-						WHERE id = %u;",
+						'UPDATE
+							runresults
+						SET
+							status = %u,
+							updated = %s
+						WHERE id = %u
+						LIMIT 1;',
 						ResultAction::$STATE_LOST,
-						$row->id
+						swarmdb_dateformat( SWARM_NOW ),
+						$resultsID
 					));
-				}
 
-				if ( $ret ) {
-					$resetTimedoutRuns++;
+					if ( $ret ) {
+						$resetTimedoutRuns++;
+					}
 				}
 			}
 		}
